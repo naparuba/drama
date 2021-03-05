25571916cb8ba6f997d2e442d6d2617583867198
Jean Gabès <naparuba@gmail.com>
2021-03-05 21:41:02 +0100

    Add: TMNT intro done

--- game/src/Screens/Title.gd
+++ game/src/Screens/Title.gd
@@ -5,17 +5,26 @@ export(String, FILE) var next_scene_path: = ""
 
 onready var player = $AnimationPlayer
 onready var mask = $mask
+onready var press_start_animation = $"push-start/push-start-animation"
+onready var title_music = $title_music
 
 func _ready():
-	self._start_animation()
+	pass #self._start_animation()
 	
 	
 func _start_animation():
+	title_music.play()
 	player.play('intro')
 
 
 func _input(event):
 	print('INPUT %s' % event)
 	if Input.is_action_just_pressed("ui_accept"):
-		print('IS PRESSED')
+		player.stop()
+		press_start_animation.play("push-start")
+		yield(press_start_animation, 'animation_finished')
 		get_tree().change_scene(next_scene_path)
+
+
+func _on_start_timer_timeout():
+	self._start_animation()
